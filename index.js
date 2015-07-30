e3bcf913f71583bfd33a5d2350e8bd3a41fa22bd
Francesco Pongiluppi <pongiluppi.f@mmfg.it>
2015-07-30 18:16:11 +0200

    More complete API, still something to fix

--- index.js
+++ index.js
@@ -16,68 +16,13 @@ function ET() {}
 
 var GT = new ET();
 
-ET.prototype._getEvents = function() {
-	return setDefault(this, '_events', {});
-}
-ET.prototype.getSubscribers = function(evtName) {
-	var events = this._getEvents();
-	return setDefault(setDefault(events, evtName, {}), 'subscribers', []);
-};
-ET.prototype.on = function(evtName, callback) {
-	/* React (callback) to a certain event (evtName).
-		Subscribers can alter the flow of the event tampering with the facade:
-		et.on('hello', function(e) {
-			e.stopped = 1; // blocks all subsequent subscribers.
-			e.prevented = 1; // blocks the defaultFn.
-		});
-	*/
-	var subs = this.getSubscribers(evtName);
-	subs.push(callback);
-};
-ET.prototype.off = function(evtName, callback) {
-	var subs = this.getSubscribers(evtName);
-	subs.splice(subs.indexOf(callback), 1);
-};
-ET.prototype._call = function(sub, args) {
-	sub.apply(this, args);
-}
-ET.prototype.fire = function(evtName /*[args, ...]*/ ) {
-	/* Fire a certain event. Subscribers will be called with
-		the event facade as first argument and any subsequent argument passed here. */
-	var args = Array.prototype.slice.call(arguments, 1),
-		events = this._getEvents();
-	// if evtName is not in _events, no subscribers are there.
-	if (owns(events, evtName)) {
-		var evt = events[evtName],
-			subs = evt.subscribers || [],
-			targets = evt.targets || [],
-			facade = {
-				name: evtName,
-				args: args,
-				target: this
-			};
-		args.unshift(facade);
-		for (var i = 0, j = subs.length; i<j; i++) {
-			this._call(subs[i], args, facade);
-			if (facade.stopped) {
-				break;
-			}
-		}
-		for (var i = 0, j = targets.length; i<j; i++) {
-			targets[i].fire.call(targets[i], arguments);
-		}
-		if (! facade.prevented && evt.defaultFn) {
-			/* if no subscriber has prevented the event, run the defaultFn (if any)*/
-			this._call(evt.defaultFn, args, facade);
-		}
-	}
-};
+/* Prepare to a certain event:
+	options = {
+		defaultFn: the function that will be called if the event cycle goes through the end,
+		broadcast: auto bubble to GlobalTarget,
+		targets: an array of EventTarget's object. The event will bubble there.
+	} */
 ET.prototype.publish = function(evtName, options) {
-	/* Prepare to a certain event:
-		options = {
-			defaultFn: the function that will be called if the event cycle goes through the end,
-			global: auto bubble to GlobalTarget
-		} */
 	var evt = {};
 	if (owns(options, 'defaultFn')) {
 		evt.defaultFn = options.defaultFn;
@@ -87,11 +32,128 @@ ET.prototype.publish = function(evtName, options) {
 	} else {
 		evt.targets = []
 	}
-	if (owns(options, 'global')) {
-		evt.targets.push(GT);
+	if (owns(options, 'broadcast')) {
+		evt['broadcast'] = options.broadcast
 	}
 	this._getEvents()[evtName] = evt;
 };
+
+ET.prototype._getEvent = function(evtName) {
+	return setDefault(this._getEvents(), evtName, {})
+}
+
+/* Returns an array with registered callbacks for the given event
+*/
+ET.prototype.getSubs = function(evtName) {
+	return setDefault(this._getEvent(evtName), 'subscribers', []);
+};
+
+/* React (callback) to a certain event (evtName). Returns an object with the method "detach" (same as et.off)
+	Subscribers can alter the flow of the event tampering with the facade:
+	et.on('hello', function(e) {
+		e.stopped = 1; // blocks all subsequent subscribers.
+		e.prevented = 1; // blocks the defaultFn.
+		e.halted = 1;  // blocks bubbling
+	});
+*/
+ET.prototype.on = function(evtName, callback) {
+	var subs = this.getSubs(evtName);
+	subs.push(callback);
+	return {
+		detach: this.off.bind(this, evtName, callback)
+	}
+};
+
+/* Remove a callback from a certain event (evtName) */
+ET.prototype.off = function(evtName, callback) {
+	var subs = this.getSubs(evtName);
+	subs.splice(subs.indexOf(callback), 1);
+};
+
+/* React just once to the given event */
+ET.prototype.once = function(evtName, callback) {
+	var ec = this.on(evtName, callback);
+	this.on(evtName, function() {
+		ec.detach();
+	});
+	return ec;
+};
+
+/* Add a bubbling target to this target.
+	All events fired on this will fire also on the bubbling targets.
+*/
+ET.prototype.addTarget = function(target) {
+	var targets = setDefault(this, '_bubblingTargets', []);
+	if (target !== this && target._receiveBubble && targets.indexOf(target) < 0) {
+		// Looks like a valid bubbling target
+		targets.push(target);
+	}
+};
+
+/* Fire a certain event. Subscribers will be called with the event facade as first argument and any subsequent argument
+	passed here.
+*/
+ET.prototype.fire = function(evtName /*[args, ...]*/ ) {
+	var args = Array.prototype.slice.call(arguments, 1),
+		facade = {
+			name: evtName,
+			target: this,
+			args: args,
+			stopped: false,
+			halted: false,
+			broadcast: false
+		};
+	args.unshift(facade);
+	this._fire(facade);
+};
+
+ET.prototype._getEvents = function() {
+	return setDefault(this, '_events', {});
+}
+ET.prototype._call = function(sub, args) {
+	sub.apply(this, args);
+}
+ET.prototype._fire = function(facade) {
+	var events = this._getEvents(),
+		targets = setDefault(this, '_bubblingTargets', []),
+		args = facade.args,
+		evtName = facade.name,
+		broadcast,
+		defaultFn;
+	// if evtName is not in _events, no subscribers are there.
+	if (owns(events, evtName)) {
+		var evt = events[evtName],
+			defaultFn = evt.defaultFn,
+			broadcast = evt.broadcast,
+			subs = evt.subscribers || [];
+		// call the subscribers
+		for (var i = 0, j = subs.length; i<j; i++) {
+			if (facade.stopped) {
+				break;
+			}
+			this._call(subs[i], args, facade);
+		}
+	}
+	// call the bubble targets
+	for (var i = 0, j = targets.length; i<j; i++) {
+		if (facade.halted) {
+			break;
+		}
+		targets[i]._receiveBubble(facade);
+	}
+	if (! facade.halted && broadcast) {
+		GT._receiveBubble(facade);
+	}
+	if (! facade.prevented && defaultFn) {
+		/* if no subscriber has prevented the event, run the defaultFn (if any)*/
+		this._call(defaultFn, args, facade);
+	}
+}
+/* Fire the same event on this target. Receive the event facade from the original event target.
+*/
+ET.prototype._receiveBubble = function(facade) {
+	this._fire(facade);
+}
 
 exports.EventTarget = ET;
 exports.GlobalTarget = GT;
--- test.js
+++ test.js
@@ -1,20 +1,58 @@
-EventTarget = require('./index').EventTarget;
+biee = require('./index');
+EventTarget = biee.EventTarget;
+GlobalTarget = biee.GlobalTarget;
 t = new EventTarget()
-t.publish('greet', {defaultFn: function() { console.log('Hello!'); }})
-t.on('greet', function(e) { console.log('To the world:'); })
-t.fire('greet');
+t2 = new EventTarget()
 
-t.publish('welcome', {
-	defaultFn: function(e, who) {
-		console.log('Welcome here, %s!', who);
-	}
+t.publish('wavehands', {
+	broadcast: true
 });
 
+GlobalTarget.on('wavehands', function(e, who) {
+	console.log('Global: someone waved hands to %s', who);
+});
+
+t.publish('greet', {
+	defaultFn: function() { console.log('t: Hello!'); },
+});
+t.on('greet', function(e) { console.log('t: To the world:'); })
+t.fire('greet');
+
+t.fire('wavehands', 'long lost pal');
+
+t.addTarget(t2);
+t.publish('welcome', {
+	defaultFn: function(e, who) {
+		console.log('t: Welcome here, %s!', who);
+	}
+});
 t.on('welcome', function(e, who) {
+	// prevent this event to bubble
+	e.halted = true;
 	if (who == 'badguy') {
 		e.prevented = true;
 	}
 });
-
+t2.on('welcome', function(e, who) {
+	throw "t2: Event " + e.name + " should not bubble here";
+});
 t.fire('welcome', 'buddy');
 t.fire('welcome', 'badguy');
+
+t2.on('blow', function(e) { console.log('t2 is reacting to a bubbling event (%s)', e.name); });
+t.fire('blow');
+
+t.once('jump', function(e) { console.log('t: jump! (you should see this log just once)')});
+t.fire('jump');
+t.fire('jump');
+
+var handler = t2.on('timeout', function() {
+	throw "t2: timeout should not be listened to";
+});
+
+setTimeout(function() { handler.detach(); }, 500);
+setTimeout(function() { t2.fire('timeout'); }, 1000);
+
+var fn = function() { throw "I detached this listener" }
+t.on('timeout', fn);
+t.off('timeout', fn);
