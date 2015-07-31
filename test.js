5fc175d11f1ef1643d78eba67b188f4e575cc84b
Francesco Pongiluppi <pongiluppi.f@mmfg.it>
2015-07-31 18:34:45 +0200

    Added some mocha tests

--- test.js
+++ test.js
@@ -4,6 +4,97 @@ var GlobalTarget = biee.GlobalTarget;
 var t = new EventTarget()
 var t2 = new EventTarget()
 
+describe('Callbacks', function() {
+	var t;
+	beforeEach(function() {
+		t = new EventTarget();
+	});
+	it('should call my callback', function(done) {
+		t.on('my-event', function(e) {
+			done();
+		});
+		t.fire('my-event');
+    });
+	it('should call my callback with my args', function(done) {
+		t.on('my-event', function(e, a, b, c) {
+			if (a == 1 && b == 'ciao' && c == null) {
+				done();
+			}
+		});
+		t.fire('my-event', 1, 'ciao', null);
+	});
+});
+
+describe('Default behaviour', function() {
+	var t;
+	beforeEach(function() {
+		t = new EventTarget();
+	});
+	it('should run the event\'s default function', function(done) {
+		t.publish('my-event', {
+			defaultFn: function(e) {
+				done();
+			}
+		});
+		t.fire('my-event');
+	});
+	it('should NOT run the event\'s default function (blocked by a subscriber)', function(done) {
+		t.publish('my-event', {
+			defaultFn: function(e) {
+				done('This shoud not be called');
+			}
+		});
+		t.on('my-event', function(e) {
+			e.preventDefault();
+		})
+		t.fire('my-event');
+	})
+	it('should run the event\'s unpreventable default function', function(done) {
+		t.publish('my-event', {
+			defaultFn: function(e) {
+				done();
+			},
+			preventable: false
+		});
+		t.on('my-event', function(e) {
+			e.preventDefault();
+		});
+		t.fire('my-event');
+	});
+});
+
+describe('Callbacks and default behaviour', function() {
+	var t;
+	beforeEach(function() {
+		t = new EventTarget();
+	});
+	it('should run the event\'s callbacks and default function, in order', function(done) {
+		var a = []
+		t.publish('my-event', {
+			defaultFn: function(e) {
+				a.push(3);
+				if (a == [1, 2, 3]) {
+					done();
+				} else {
+					done({
+						'message': 'Callbacks not ordered',
+						'callbacks': a
+					})
+				}
+				
+			}
+		});
+		t.on('my-event', function(e) {
+			a.push(1);
+		});
+		t.on('my-event', function(e) {
+			a.push(2);
+		});
+		t.fire('my-event');
+	});
+	
+})
+
 t.publish('wavehands', {
 	broadcast: true
 });
